a5aa153c3ef35cf0249baa2b2403a061940ecb74
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-08-20 01:58:47 +0530

    route protection for logged in user added

--- src/frontend/src/Pages/Auth & Authorization/ProtectedRoutes.jsx
+++ src/frontend/src/Pages/Auth & Authorization/ProtectedRoutes.jsx
@@ -1,18 +1,18 @@
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 // import { AuthContext } from "./AuthContext";
 import Cookie from "js-cookie";
 
 export default function ProtectedRoutes() {
   // const { user } = useContext(AuthContext);
   const isLoggedIn = Cookie.get("token");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // use this componet for redirecting unauthenticated users
   useEffect(() => {
-    // if (!isLoggedIn) {
-    //   return navigate("/", { replace: false });
-    // }
+    if (!isLoggedIn) {
+      return navigate("/", { replace: false });
+    }
   }, [isLoggedIn]);
 
   return (
