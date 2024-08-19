c02c8cffc75f6649a9e92c03bc0fc6d7dce751e2
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-08-19 19:10:51 +0530

    backend removed

--- src/backend/Routes/StudentRoutes.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-export const Router = express.Router();
-import {
-	cookieCheckerMiddleware,
-} from "../Middlewares/CookieChecker";
-// Router.route("/signup").post(signup);
