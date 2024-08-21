76511e28c0dce6abde35a91804f0d7cfb78db957
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-08-22 02:40:32 +0530

    admin layout done

--- src/frontend/src/Pages/Admin/AdminLayout/BottomBar.jsx
+++ src/frontend/src/Pages/Admin/AdminLayout/BottomBar.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import BottomDrawer from "./MobileDrawer";
 import { FaHome, FaSearch } from "react-icons/fa";
-import { MdGroups } from "react-icons/md";
+import { FaUserGroup } from "react-icons/fa6";
+import MobileDrawer from "./MobileDrawer";
+import { HiAdjustments } from "react-icons/hi";
 
 export default function BottomBar() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
@@ -15,65 +16,25 @@ export default function BottomBar() {
           isDrawerOpen ? "hidden" : "fixed"
         } sm:hidden  z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600`}
       >
-        <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
+        <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
+          {/* button 1 */}
           <BottomBarItem
             icon={<FaHome className={iconCss} size={25} />}
             text={"home"}
           />
+          {/* button 2 */}
 
           <BottomBarItem
             icon={<FaSearch className={iconCss} size={25} />}
             text={"Search"}
           />
+          {/* button 3 */}
 
           <BottomBarItem
-            icon={<MdGroups className={iconCss} size={25} />}
-            text={"Search"}
+            icon={<FaUserGroup className={iconCss} size={25} />}
+            text={"Committee"}
           />
-          {/* 
-          <div className="flex items-center justify-center">
-            <button
-              data-tooltip-target="tooltip-new"
-              type="button"
-              className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
-            >
-              <svg
-                className="w-4 h-4 text-white"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M9 1v16M1 9h16"
-                />
-              </svg>
-              <span className="sr-only">New item</span>
-            </button>
-          </div> */}
-
-          <button
-            data-tooltip-target="tooltip-settings"
-            type="button"
-            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-          >
-            <svg
-              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-            </svg>
-            <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-              Profile
-            </span>
-          </button>
+          {/* button 4 */}
 
           <button
             onClick={() => setIsDrawerOpen(true)}
@@ -81,28 +42,14 @@ export default function BottomBar() {
             type="button"
             className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
           >
-            <svg
-              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"
-              />
-            </svg>
+            <HiAdjustments className={iconCss} size={25} />
             <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-              Settings
+              More
             </span>
           </button>
         </div>
       </div>
-      <BottomDrawer
+      <MobileDrawer
         isDrawerOpen={isDrawerOpen}
         setIsDrawerOpen={setIsDrawerOpen}
       />
