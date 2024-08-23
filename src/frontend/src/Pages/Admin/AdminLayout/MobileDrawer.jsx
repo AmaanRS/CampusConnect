37a259d38bf34239626af27f397f26b4ae358b01
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-08-23 10:38:21 +0530

    routes updated

--- src/frontend/src/Pages/Admin/AdminLayout/MobileDrawer.jsx
+++ src/frontend/src/Pages/Admin/AdminLayout/MobileDrawer.jsx
@@ -42,45 +42,30 @@ export default function MobileDrawer({ isDrawerOpen, setIsDrawerOpen }) {
                 </form>
                 <Sidebar.Items>
                   <Sidebar.ItemGroup>
-                    <Sidebar.Item href="/" icon={HiChartPie}>
+                    <Sidebar.Item href="" icon={HiChartPie}>
                       Dashboard
                     </Sidebar.Item>
-                    <Sidebar.Item
-                      href="/e-commerce/products"
-                      icon={HiShoppingBag}
-                    >
+                    <Sidebar.Item href="/" icon={HiShoppingBag}>
                       Products
                     </Sidebar.Item>
-                    <Sidebar.Item href="/users/list" icon={HiUsers}>
+                    <Sidebar.Item href="/" icon={HiUsers}>
                       Users list
                     </Sidebar.Item>
-                    <Sidebar.Item href="/authentication/sign-in" icon={HiLogin}>
+                    <Sidebar.Item href="/" icon={HiLogin}>
                       Sign in
                     </Sidebar.Item>
-                    <Sidebar.Item
-                      href="/authentication/sign-up"
-                      icon={HiPencil}
-                    >
+                    <Sidebar.Item href="" icon={HiPencil}>
                       Sign up
                     </Sidebar.Item>
                   </Sidebar.ItemGroup>
                   <Sidebar.ItemGroup>
-                    <Sidebar.Item
-                      href="https://github.com/themesberg/flowbite-react/"
-                      icon={HiClipboard}
-                    >
+                    <Sidebar.Item href="" icon={HiClipboard}>
                       Docs
                     </Sidebar.Item>
-                    <Sidebar.Item
-                      href="https://flowbite-react.com/"
-                      icon={HiCollection}
-                    >
+                    <Sidebar.Item href="" icon={HiCollection}>
                       Components
                     </Sidebar.Item>
-                    <Sidebar.Item
-                      href="https://github.com/themesberg/flowbite-react/issues"
-                      icon={HiInformationCircle}
-                    >
+                    <Sidebar.Item href="" icon={HiInformationCircle}>
                       Help
                     </Sidebar.Item>
                   </Sidebar.ItemGroup>
