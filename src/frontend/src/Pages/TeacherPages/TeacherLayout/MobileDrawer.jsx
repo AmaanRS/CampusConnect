35bf49c3e57a54d118d3b48341714172e77b71aa
saurabh-rana17 <saurabhrana200317@gmail.com>
2024-09-08 18:41:49 +0530

    MobileDrawer Improved

--- src/frontend/src/Pages/TeacherPages/TeacherLayout/MobileDrawer.jsx
+++ src/frontend/src/Pages/TeacherPages/TeacherLayout/MobileDrawer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 "use client";
 
-import { Button, Drawer, Sidebar, TextInput } from "flowbite-react";
+import { Drawer, Sidebar } from "flowbite-react";
 import { useContext } from "react";
 import { FiLogOut } from "react-icons/fi";
 import {
@@ -11,19 +11,27 @@ import {
   HiInformationCircle,
   HiLogin,
   HiPencil,
-  HiSearch,
   HiShoppingBag,
   HiUsers,
 } from "react-icons/hi";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { UserContext } from "../../../store/UserContextProvider";
 
 export default function MobileDrawer({ isDrawerOpen, setIsDrawerOpen }) {
+  const { pathname } = useLocation();
+  const currntPage = pathname.split("/").pop();
   const { logOutUser } = useContext(UserContext);
   const handleClose = () => setIsDrawerOpen(false);
-  const sidebarItemStyle =
-    "transition-colors group focus:bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800";
 
+  function getStyle(path) {
+    const sidebarItemStyle =
+      "transition-colors group focus:bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800 ";
+    const activeStyling =
+      "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800";
+    const finalStyle =
+      sidebarItemStyle + `${currntPage === path ? activeStyling : ""}`;
+    return finalStyle;
+  }
   return (
     <>
       <Drawer open={isDrawerOpen} onClose={handleClose}>
@@ -34,59 +42,39 @@ export default function MobileDrawer({ isDrawerOpen, setIsDrawerOpen }) {
             className="[&>div]:bg-transparent [&>div]:p-0"
           >
             <div className="flex h-full flex-col justify-between py-2">
-              <div>
-                <form className="pb-3 md:hidden">
-                  <TextInput
-                    icon={HiSearch}
-                    type="search"
-                    placeholder="Search"
-                    required
-                    size={32}
-                  />
-                </form>
-                <Sidebar.Items>
-                  <Sidebar.ItemGroup>
-                    <Sidebar.Item
-                      className={sidebarItemStyle}
-                      as={NavLink}
-                      to="/teacher"
-                      icon={HiChartPie}
-                      onClick={() => handleClose()}
-                    >
-                      Dashboard
-                    </Sidebar.Item>
-                    <Sidebar.Item
-                      as={NavLink}
-                      className={sidebarItemStyle}
-                      to="createcommittee"
-                      icon={HiUsers}
-                      onClick={() => handleClose()}
-                    >
-                      Create Committee
-                    </Sidebar.Item>
-                    <Sidebar.Item
-                      className={sidebarItemStyle}
-                      icon={HiShoppingBag}
-                    >
-                      Users list
-                    </Sidebar.Item>
-                    <Sidebar.Item className={sidebarItemStyle} icon={HiLogin}>
-                      Sign in
-                    </Sidebar.Item>
-                    <Sidebar.Item className={sidebarItemStyle} icon={HiPencil}>
-                      Sign up
-                    </Sidebar.Item>
-                  </Sidebar.ItemGroup>
-                  <Sidebar.ItemGroup>
-                    <Sidebar.Item icon={HiClipboard}>Docs</Sidebar.Item>
-                    <Sidebar.Item icon={HiCollection}>Components</Sidebar.Item>
-                    <Sidebar.Item icon={HiInformationCircle}>Help</Sidebar.Item>
-                    <Sidebar.Item onClick={() => logOutUser()} icon={FiLogOut}>
-                      Logout
-                    </Sidebar.Item>
-                  </Sidebar.ItemGroup>
-                </Sidebar.Items>
-              </div>
+              <Sidebar.Items>
+                <Sidebar.ItemGroup>
+                  <Sidebar.Item
+                    className={getStyle("teacher")}
+                    as={NavLink}
+                    to="/teacher"
+                    icon={HiChartPie}
+                    onClick={() => handleClose()}
+                  >
+                    Dashboard
+                  </Sidebar.Item>
+                  <Sidebar.Item
+                    as={NavLink}
+                    className={getStyle("createcommittee")}
+                    to="createcommittee"
+                    icon={HiUsers}
+                    onClick={() => handleClose()}
+                  >
+                    Create Committee
+                  </Sidebar.Item>
+                  <Sidebar.Item icon={HiShoppingBag}>Users list</Sidebar.Item>
+                  <Sidebar.Item icon={HiLogin}>Sign in</Sidebar.Item>
+                  <Sidebar.Item icon={HiPencil}>Sign up</Sidebar.Item>
+                </Sidebar.ItemGroup>
+                <Sidebar.ItemGroup>
+                  <Sidebar.Item icon={HiClipboard}>Docs</Sidebar.Item>
+                  <Sidebar.Item icon={HiCollection}>Components</Sidebar.Item>
+                  <Sidebar.Item icon={HiInformationCircle}>Help</Sidebar.Item>
+                  <Sidebar.Item onClick={() => logOutUser()} icon={FiLogOut}>
+                    Logout
+                  </Sidebar.Item>
+                </Sidebar.ItemGroup>
+              </Sidebar.Items>
             </div>
           </Sidebar>
         </Drawer.Items>
