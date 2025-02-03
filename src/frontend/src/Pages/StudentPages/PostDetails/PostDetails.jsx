298819a0ea8b2e96c1a3efc283dc4a6442c32966
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-04 00:04:56 +0530

    postTitle completed

--- src/frontend/src/Pages/StudentPages/PostDetails/PostDetails.jsx
+++ src/frontend/src/Pages/StudentPages/PostDetails/PostDetails.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PostTop from "./PostTop";
+import PostTitle from "./PostTitle";
 
 export default function PostDetails() {
   return (
     <>
       <div className=" h-screen max-w-3xl m-auto">
         <PostTop />
+        <PostTitle />
       </div>
     </>
   );
---
+++ src/frontend/src/Pages/StudentPages/PostDetails/PostTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PostTitle() {
+  const title =
+    "React  Facing trouble to store data in form of array of objects which is coming from my spring boot api.";
+  return (
+    <>
+      <div className="mt-1 font-bold text-2xl leading-7">{title}</div>
+    </>
+  );
+}
