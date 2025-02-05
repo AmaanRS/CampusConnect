0e610713c37fe13a9f8d957c3faa263f37a7c236
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-05 19:10:44 +0530

    committee sidebar done

--- src/frontend/src/Pages/StudentPages/Components/CommiteeSidebarMemberList.jsx
+++ src/frontend/src/Pages/StudentPages/Components/CommiteeSidebarMemberList.jsx
@@ -1,5 +1,22 @@
 import React from "react";
-
+import { Avatar } from "flowbite-react";
 export default function CommiteeSidebarMemberList() {
-  return <div>CommiteeSidebarMemberList</div>;
+  return (
+    <div className="px-4 mb-4">
+      <p className="text-sm font-medium text-slate-700  mb-2">Members</p>
+      <div>
+        <MemberList />
+        <MemberList />
+      </div>
+    </div>
+  );
+}
+
+function MemberList() {
+  return (
+    <div className="flex py-1 px-1 rounded-lg transition-colors duration-200 my-1 items-center hover:bg-slate-200">
+      <Avatar rounded size="xs" />
+      <div className="text-slate-700 text-xs ml-2 font-medium">Username</div>
+    </div>
+  );
 }
