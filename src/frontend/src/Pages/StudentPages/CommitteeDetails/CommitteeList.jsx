383c4f028f78a4ccbdfb5ac2ca8fdca61dcdd192
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-06 01:06:28 +0530

    committee detail enhanced

--- src/frontend/src/Pages/StudentPages/CommitteeDetails/CommitteeList.jsx
+++ src/frontend/src/Pages/StudentPages/CommitteeDetails/CommitteeList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import StudentPost from "../Components/PostPreview/Post/StudentPost";
+import { Avatar } from "flowbite-react";
 
 export default function CommitteeList() {
   return (
     <>
-      <p className="mb-4 ml-2 font-bold text-2xl">Committee Name</p>
+      <div className="flex bg-slate-100 rounded-lg items-center mb-4 px-4 ">
+        <Avatar size="lg" rounded className="  rounded-full" />
+        <p className="ml-2 font-bold text-2xl">Committee Name</p>
+      </div>
       <div className="max-w-3xl m-auto">
         <hr className="mb-2 mx-1" />
         <StudentPost mode="committee" />
