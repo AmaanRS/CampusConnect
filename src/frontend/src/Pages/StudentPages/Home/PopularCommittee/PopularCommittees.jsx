1c7edbd6f26220f46afb765d762b52b19475954e
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-05 11:40:50 +0530

    refactord popular committee sidebar

--- src/frontend/src/Pages/StudentPages/Home/PopularCommittee/PopularCommittees.jsx
+++ src/frontend/src/Pages/StudentPages/Home/PopularCommittee/PopularCommittees.jsx
@@ -3,7 +3,7 @@ import PopularCommiteeItem from "./PopularCommiteeItem";
 
 export default function PopularCommittees() {
   return (
-    <div className="bg-slate-50 py-3 mt-4 mr-2 pl-2 rounded-lg">
+    <div className="bg-slate-50 py-3 mt-4  pl-2 rounded-lg">
       <h1 className="font-semibold text-sm mb-6 text-gray-700">
         Popular Committees
       </h1>
