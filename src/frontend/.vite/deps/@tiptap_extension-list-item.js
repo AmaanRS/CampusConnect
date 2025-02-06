2369c795b88c34f2065932f385f41cc7a2288948
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-06 23:27:31 +0530

    updated vite deps

---
+++ src/frontend/.vite/deps/@tiptap_extension-list-item.js
@@ -0,0 +1,10 @@
+import {
+  ListItem
+} from "./chunk-6TWD36GW.js";
+import "./chunk-7BOXJOEQ.js";
+import "./chunk-HXA6O6EE.js";
+export {
+  ListItem,
+  ListItem as default
+};
+//# sourceMappingURL=@tiptap_extension-list-item.js.map
---
+++ src/frontend/.vite/deps/@tiptap_extension-text-style.js
@@ -0,0 +1,10 @@
+import {
+  TextStyle
+} from "./chunk-OVNWVXZZ.js";
+import "./chunk-7BOXJOEQ.js";
+import "./chunk-HXA6O6EE.js";
+export {
+  TextStyle,
+  TextStyle as default
+};
+//# sourceMappingURL=@tiptap_extension-text-style.js.map
