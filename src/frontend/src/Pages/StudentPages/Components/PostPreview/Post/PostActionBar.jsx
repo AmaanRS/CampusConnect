a4c8c8fc09ddff7344d684e256d6dd26c6c50b31
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-10 17:09:54 +0530

    Post action link issue resolved

--- src/frontend/src/Pages/StudentPages/Components/PostPreview/Post/PostActionBar.jsx
+++ src/frontend/src/Pages/StudentPages/Components/PostPreview/Post/PostActionBar.jsx
@@ -22,7 +22,7 @@ export default function PostActionBar({ postId }) {
   return (
     <div className="flex">
       {/* like button */}
-      <Link to={"/student/post/1"}>
+      <Link to={`/student/post/${postId}`}>
         <div
           className={`min-w-16   rounded-full flex  items-center justify-center py-0.5 pl-1 pr-3 mb-1 mt-1 ${
             active ? "text-white bg-blue-600 " : "bg-slate-200"
@@ -46,7 +46,7 @@ export default function PostActionBar({ postId }) {
 
       {/* comment button */}
       <Link
-        to={"/student/post/1"}
+        to={`/student/post/${postId}`}
         className="min-w-16 ml-3  bg-slate-200 rounded-full flex  items-center justify-center py-0.5 pl-1 pr-3 mb-1 mt-1"
       >
         <button
