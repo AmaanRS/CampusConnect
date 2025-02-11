4344df1674502d44a45dfc8755d95eba2b1eb3f2
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-11 19:32:22 +0530

    toastify improved

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -138,7 +138,7 @@ function App() {
             newestOnTop={false}
             closeOnClick
             rtl={false}
-            pauseOnFocusLoss
+            pauseOnFocusLoss={false}
             draggable
             pauseOnHover={false}
             theme="light"
