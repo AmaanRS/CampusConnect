2c6f12f8264d304de35be327383c7685a904c778
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-02-12 19:45:06 +0530

    committecategorised

--- src/frontend/src/Pages/AdminPages/AllCommittees/AllCommittees.jsx
+++ src/frontend/src/Pages/AdminPages/AllCommittees/AllCommittees.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axiosInstance from "../../../utils/Axios/AxiosInstance";
 import ApiError from "../../../Components/Errors/ApiError";
 import AllCommitteeCard from "./AllCommitteeCard";
@@ -7,29 +7,64 @@ import AllCommitteesCardSkeleton from "./AllCommitteesCardSkeleton";
 import CentreMainContent from "../../../Components/Layout/Desktop/CentreMainContent";
 import RightSidebar from "../../../Components/Layout/Desktop/RightSidebar";
 import PopularCommittees from "../../StudentPages/Home/PopularCommittee/PopularCommittees";
+import { Tabs } from "flowbite-react";
 
 export default function AllCommittees() {
-  const committeRequest = useQuery({
+  const {
+    data: committees = [],
+    isError,
+    error,
+    isLoading,
+  } = useQuery({
     queryKey: ["allCommittee"],
     queryFn: () => axiosInstance.post("/committee/getAllCommittees"),
   });
+  const [list, setList] = useState({
+    acceptedCommittees: [],
+    pendingCommittees: [],
+    deletedCommittees: [],
+  });
 
-  if (committeRequest.isError) {
-    console.log(committeRequest.error);
+  useEffect(() => {
+    if (committees?.data?.data?.length > 0) {
+      const allArray = committees?.data?.data;
 
-    return (
-      <ApiError
-        error={committeRequest.error}
-        isError={committeRequest.isError}
-      />
-    );
+      const acceptedCommittees = allArray.filter(
+        (committee) => committee.status === "ACCEPTED"
+      );
+      const pendingCommittees = allArray.filter(
+        (committee) => committee.status === "PENDING"
+      );
+      const deletedCommittees = allArray.filter(
+        (committee) => committee.status === "DELETED"
+      );
+
+      const categorisedList = {
+        acceptedCommittees,
+        pendingCommittees,
+        deletedCommittees,
+      };
+      setList(categorisedList);
+    }
+  }, [committees]);
+
+  if (isError) {
+    console.log(error);
+
+    return <ApiError error={error} isError={isError} />;
   }
 
-  // if (committeRequest.isLoading) {
-  //   return <>Loading</>;
-  // }
-
-  console.log(committeRequest?.data?.data?.data);
+  if (isLoading) {
+    return (
+      <>
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 m-auto max-w-xl mb-4">
+          <AllCommitteesCardSkeleton />
+          <AllCommitteesCardSkeleton />
+          <AllCommitteesCardSkeleton />
+        </div>
+      </>
+    );
+  }
 
   // return <>Working</>;
 
@@ -37,42 +72,72 @@ export default function AllCommittees() {
     <>
       <CentreMainContent>
         <div className=" px-2 ">
-          {(committeRequest.isLoading ||
-            committeRequest?.data?.data?.data.length !== 0) && (
+          {(isLoading || committees?.data?.data?.length !== 0) && (
             <p className="mb-3 md:text-2xl text-center font-bold text-xl ">
               All Committees
             </p>
           )}
-          {committeRequest.isLoading && (
-            <>
-              <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 m-auto max-w-xl mb-4">
-                <AllCommitteesCardSkeleton />
-                <AllCommitteesCardSkeleton />
-                <AllCommitteesCardSkeleton />
-              </div>
-            </>
-          )}
-          {committeRequest?.data?.data?.data.length !== 0 && (
-            <>
-              <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1  gap-4">
-                {committeRequest?.data?.data?.data.map((item) => {
-                  return (
-                    <AllCommitteeCard key={item.committeeId} item={item} />
-                  );
-                })}
-              </div>
-            </>
-          )}
-          {committeRequest?.data?.data?.data.length === 0 && (
+          {committees?.data?.data.length === 0 && (
             <p className="text-2xl font-bold text-center  border-2   w-fit m-auto p-4 rounded-md border-slate-400">
               There are no Pending Requests for New Committee
             </p>
           )}
+
+          {/* tabs */}
+          <>
+            <Tabs aria-label="Default tabs" variant="default">
+              <Tabs.Item active title="Pending">
+                {list?.pendingCommittees?.length !== 0 && (
+                  <>
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1  gap-4">
+                      {list.pendingCommittees.map((item) => {
+                        return (
+                          <AllCommitteeCard
+                            key={item.committeeId}
+                            item={item}
+                          />
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+              </Tabs.Item>
+              <Tabs.Item title="Active">
+                {list?.acceptedCommittees?.length !== 0 && (
+                  <>
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1  gap-4">
+                      {list.acceptedCommittees.map((item) => {
+                        return (
+                          <AllCommitteeCard
+                            key={item.committeeId}
+                            item={item}
+                          />
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+              </Tabs.Item>
+              <Tabs.Item title="Deleted">
+                {list?.deletedCommittees?.length !== 0 && (
+                  <>
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1  gap-4">
+                      {list.deletedCommittees.map((item) => {
+                        return (
+                          <AllCommitteeCard
+                            key={item.committeeId}
+                            item={item}
+                          />
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+              </Tabs.Item>
+            </Tabs>
+          </>
         </div>
       </CentreMainContent>
-      <RightSidebar>
-        <PopularCommittees />
-      </RightSidebar>
     </>
   );
 }
