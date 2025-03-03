e739415f55a1b323cd35f9aacb4e81d8ff7293bb
saurabh-rana17 <saurabhrana200317@gmail.com>
2025-03-03 17:29:58 +0530

    styling improved

--- src/frontend/src/Pages/StudentPages/Explore/AllCommitteeItem.jsx
+++ src/frontend/src/Pages/StudentPages/Explore/AllCommitteeItem.jsx
@@ -1,31 +1,35 @@
 import { Avatar } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AllCommitteeItem({ item }) {
   console.log(item);
   return (
-    <div className="border hover:bg-slate-50 cursor-pointer transition-colors duration-200 p-2 rounded-xl border-slate-300">
+    <div className="border hover:bg-slate-50 cursor-pointer transition-colors duration-200 p-4 rounded-xl border-slate-300 shadow-sm hover:shadow-lg">
       <div className="flex w-full justify-between">
-        <div className="flex">
+        <div className="flex items-center">
           <Avatar className="text-slate-400" size={"md"} rounded />
-          <div className="ml-3 min-w-0">
-            <p className="font-bold   text-sm text-slate-700">{item?.name}</p>
-
-            <p className="text-xs text-slate-600">
+          <div className="ml-4 min-w-0">
+            <p className="font-semibold text-sm text-slate-800 hover:text-blue-700">
+              {item?.name}
+            </p>
+            <p className="text-xs text-slate-500">
               {item?.facultyTeam?.length + item?.members?.length} members
             </p>
           </div>
         </div>
+
+        {/* follow button */}
         <div>
           <button
             type="button"
-            className=" bg-blue-800 hover:bg-blue-900 font-medium text-sm py-1 px-2 rounded-full text-white"
+            className="bg-blue-700 hover:bg-blue-800 font-medium text-sm py-2 px-4 rounded-full text-white transition-colors duration-200"
           >
             Follow
           </button>
         </div>
       </div>
-      <div className="line-clamp-2 font- text-xs mt-2 text-slate-600">
+      <div className="line-clamp-2 mt-3 text-xs text-slate-600 font-normal">
         {item?.description}
       </div>
     </div>
